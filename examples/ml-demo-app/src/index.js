03c7df03239cfd56b9a1a0cade0a3c60ed3f6395
wooldridge <mike@hellion.org>
2023-03-01 12:59:03 -0800

    Add context

--- examples/ml-demo-app/src/index.js
+++ examples/ml-demo-app/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MLProvider } from "./MLContext";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <MLProvider
+    scheme="http"
+    host="localhost"
+    port="4000"
+  >
     <App />
-  </React.StrictMode>
+  </MLProvider>
 );
